e45bfbbd616838eb1b1caf1aafefdf7cdf5e3f8f
ngochieu1297 <ngochieu1297@example.com>
2019-03-01 23:07:25 +0700

    Create_base_model

--- app/src/main/java/com/example/music_58/data/repository/TrackRepository.java
+++ app/src/main/java/com/example/music_58/data/repository/TrackRepository.java
@@ -1,5 +1,69 @@
 package com.example.music_58.data.repository;
 
-public class TrackRepository {
+import com.example.music_58.data.model.Track;
+import com.example.music_58.data.source.TrackDataSource;
 
+public class TrackRepository implements TrackDataSource.Local, TrackDataSource.Remote {
+    private static TrackRepository sInstance;
+    private TrackDataSource.Remote mRemoteDataSource;
+    private TrackDataSource.Local mLocalDataSource;
+
+    private TrackRepository(TrackDataSource.Remote remoteDataSource,
+                            TrackDataSource.Local localDataSource) {
+        mRemoteDataSource = remoteDataSource;
+        mLocalDataSource = localDataSource;
+    }
+
+    public static synchronized TrackRepository getInstance(TrackDataSource.Remote remoteDataSource,
+                                                           TrackDataSource.Local localDataSource) {
+        if (sInstance == null) {
+            sInstance = new TrackRepository(remoteDataSource, localDataSource);
+        }
+        return sInstance;
+    }
+
+    @Override
+    public void getLocalTracks(DataCallback<Track> callback) {
+        mLocalDataSource.getLocalTracks(callback);
+    }
+
+    @Override
+    public void searchLocalTracks(String keyword, DataCallback<Track> callback) {
+        mLocalDataSource.searchLocalTracks(keyword, callback);
+    }
+
+    @Override
+    public void getFavoriteTracks(DataCallback<Track> callback) {
+        mLocalDataSource.getFavoriteTracks(callback);
+    }
+
+    @Override
+    public void addFavoriteTrack(Track track, DataCallback<Boolean> callback) {
+        mLocalDataSource.addFavoriteTrack(track, callback);
+    }
+
+    @Override
+    public void deleteFavoriteTrack(Track track, DataCallback<Boolean> callback) {
+        mLocalDataSource.deleteFavoriteTrack(track, callback);
+    }
+
+    @Override
+    public void getRecentTrack(DataCallback<Long> callback) {
+        mLocalDataSource.getRecentTrack(callback);
+    }
+
+    @Override
+    public void getTracks(String api, DataCallback<Track> callback) {
+        mRemoteDataSource.getTracks(api, callback);
+    }
+
+    @Override
+    public void searchTracks(String api, DataCallback<Track> callback) {
+        mRemoteDataSource.searchTracks(api, callback);
+    }
+
+    @Override
+    public void getDetailTrack(String api, DataCallback<Track> callback) {
+        mRemoteDataSource.getDetailTrack(api, callback);
+    }
 }
--- app/src/main/java/com/example/music_58/data/source/remote/TrackRemoteDataSource.java
+++ app/src/main/java/com/example/music_58/data/source/remote/TrackRemoteDataSource.java
@@ -1,5 +1,30 @@
 package com.example.music_58.data.source.remote;
 
-public class TrackRemoteDataSource {
+import com.example.music_58.data.model.Track;
+import com.example.music_58.data.source.TrackDataSource;
 
+public class TrackRemoteDataSource implements TrackDataSource.Remote {
+    private static TrackRemoteDataSource sInstance;
+
+    public static synchronized TrackRemoteDataSource getInstance(){
+        if(sInstance == null) {
+            sInstance = new TrackRemoteDataSource();
+        }
+        return sInstance;
+    }
+
+    @Override
+    public void getTracks(String api, DataCallback<Track> callback) {
+
+    }
+
+    @Override
+    public void searchTracks(String api, DataCallback<Track> callback) {
+
+    }
+
+    @Override
+    public void getDetailTrack(String api, DataCallback<Track> callback) {
+
+    }
 }
