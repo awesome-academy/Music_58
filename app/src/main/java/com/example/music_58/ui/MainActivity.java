112cf20f9cb11b1af0e0e58b430d8c4e07b6f317
ngochieu1297 <ngochieu1297@example.com>
2019-03-03 12:07:12 +0700

    Create favorite screen

--- app/src/main/java/com/example/music_58/ui/MainActivity.java
+++ app/src/main/java/com/example/music_58/ui/MainActivity.java
@@ -1,18 +1,22 @@
 package com.example.music_58.ui;
 
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.music_58.R;
 import com.example.music_58.ui.adapter.ViewPagerAdapter;
+import com.example.music_58.ui.search.SearchActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private TabLayout mTabLayout;
     private ViewPager mViewPager;
-    private Toolbar mToolbar;
+    private ImageView mImageSearch;
+    private TextView mTextSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initView();
         addEvent();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.text_search:
+                startActivity(SearchActivity.getIntent(this));
+                break;
+            case R.id.image_search:
+                startActivity(SearchActivity.getIntent(this));
+                break;
+            default:
+                break;
+        }
     }
 
     private void addEvent() {
@@ -42,12 +60,14 @@ public class MainActivity extends AppCompatActivity {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         mViewPager.setAdapter(adapter);
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+        mImageSearch.setOnClickListener(this);
+        mTextSearch.setOnClickListener(this);
     }
 
     private void initView() {
-        getSupportActionBar().hide();
         mTabLayout = findViewById(R.id.tab_layout);
         mViewPager = findViewById(R.id.view_pager);
-        mToolbar = findViewById(R.id.tool_bar);
+        mImageSearch = findViewById(R.id.image_search);
+        mTextSearch = findViewById(R.id.text_search);
     }
 }
